365494917503f51dd5ac78dc8e0c7e3ef5ade17a
NathanielSnow <sno20001@byui.edu>
2021-09-17 16:44:58 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
-let links = ["face"];
-
-/*const links = [
+const links = [
   {
     label: "Week1 notes",
     url: "seethesound.org",
   },
-];*/
+];
 
 var ol = document.getElementById("exercise_list");
 
 for (var i = 0; i < links.length; i++) {
-  var link = linkList[i];
+  var link = links[i];
   var li = document.createElement("li");
   li.appendChild(document.createTextNode(link));
   ol.appendChild(li);
