9022412071c90f380861ac305d0c72ecb34b2d8b
NathanielSnow <sno20001@byui.edu>
2021-09-17 16:50:09 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@ let links = ["Nate", "John"];
 
 var ol = document.getElementById("exercise_list");
 
-for (var i = 0; i < links.length; i++) {
-  var link = links[i];
-  var li = document.createElement("li");
-  li.appendChild(document.createTextNode(link));
-  ol.appendChild(li);
-}
+document.addEventListener("DOMContentLoaded", function () {
+  // this function runs when the DOM is ready, i.e. when the document has been parsed
+  for (var i = 0; i < links.length; i++) {
+    var link = links[i];
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(link));
+    ol.appendChild(li);
+  }
+});
