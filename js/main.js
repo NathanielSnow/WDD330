c4442127eb2d63faa37083470f8f4ce7baa6e03f
NathanielSnow <sno20001@byui.edu>
2021-09-17 17:21:39 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 const links = [
   {
     label: "Week1 notes",
-    url: "week1/index.html",
+    url: "seethesound.org",
   },
   {
     label: "Week2 notes",
-    url: "week2/index.html",
+    url: "youtube.com",
   },
 ];
 
