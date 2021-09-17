3480f7ae3325536e1a8e49ce2630c77812e20f60
NathanielSnow <sno20001@byui.edu>
2021-09-17 16:52:31 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ var ol = document.getElementById("exercise_list");
 document.addEventListener("DOMContentLoaded", function () {
   // this function runs when the DOM is ready, i.e. when the document has been parsed
   for (var i = 0; i < links.length; i++) {
-    var link = links[i];
+    var link = links[i].label;
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(link));
     ol.appendChild(li);
