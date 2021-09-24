424add9e7ebd71af1445592c2015afa598a29a16
NathanielSnow <sno20001@byui.edu>
2021-09-23 21:25:41 -0500

    Update teamAssignment.js

--- Week 2/exercises/js/teamAssignment.js
+++ Week 2/exercises/js/teamAssignment.js
@@ -8,28 +8,32 @@ function exercise1() {
   showText(txtValue, "txtOutput");
 }
 
-//Takes input number and
+//Takes input number and add it along with each number that proceeds it
 function exercise2() {
   const value2 = document.querySelector("#number").value;
   let result = sumNumberUntil(value2);
   showText(result, "txtOutput2");
 }
 
+//adds two numbers and displays the sum in a div
 function exercise3() {
   let result = parseFloat(number1) + parseFloat(number2);
   showText(result, "txtOutput3");
 }
 
+//multiplies two numbers and displays the product in a div
 const multiply = () => {
   let result = parseFloat(number1) * parseFloat(number2);
   showText(result, "txtOutput3");
 };
 
+//subtracts two numbers and displays the difference in a div
 const substract = function () {
   let result = parseFloat(number1) - parseFloat(number2);
   showText(result, "txtOutput3");
 };
 
+//divides two numbers and displays the quotient in a div; checks to see if the second number is a zero
 function divide() {
   if (number2 === 0) {
     alert("You can't divide by zero!");
@@ -45,6 +49,7 @@ function showText(what, where) {
   txtOutput.innerHTML = what;
 }
 
+//adds a sequence of numbers
 function sumNumberUntil(number) {
   let total = 0;
   for (i = 1; i <= number; i++) {
