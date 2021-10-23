d18d6e73e440d78743dd947967e53ddf03266e91
NathanielSnow <sno20001@byui.edu>
2021-10-23 03:34:38 -0500

    Update main.js

--- Week 6/js/main.js
+++ Week 6/js/main.js
@@ -19,13 +19,68 @@ function displayList() {
     let row = tasks.insertRow(rowCount);
 
     let cell1 = row.insertCell(0);
-    cell1.innerHTML = '<input type="checkbox" onclick="updateTask()">';
-
     let cell2 = row.insertCell(1);
-    cell2.innerHTML = "<p>" + toDoList[x].content + "</p>";
+
+    if (toDoList[x].completed) {
+      cell1.innerHTML =
+        '<input type="checkbox" onclick="updateTask()" checked>';
+      cell2.innerHTML =
+        '<p class="completedTask">' + toDoList[x].content + "</p>";
+    } else {
+      cell1.innerHTML = '<input type="checkbox" onclick="updateTask()">';
+      cell2.innerHTML = "<p>" + toDoList[x].content + "</p>";
+    }
 
     let cell3 = row.insertCell(2);
     cell3.innerHTML = '<button onclick="deleteTask(this)">&#x2715</button>';
+  }
+  statusBar();
+}
+
+function showActive() {
+  let tasks = document.getElementById("taskList");
+  tasks.innerHTML = "";
+  for (let x = 0; x < toDoList.length; x++) {
+    let tasks = document.getElementById("taskList");
+
+    let rowCount = tasks.rows.length;
+    let row = tasks.insertRow(rowCount);
+
+    if (!toDoList[x].completed) {
+      let cell1 = row.insertCell(0);
+      cell1.innerHTML = '<input type="checkbox" onclick="updateTask()">';
+
+      let cell2 = row.insertCell(1);
+      cell2.innerHTML = "<p>" + toDoList[x].content + "</p>";
+
+      let cell3 = row.insertCell(2);
+      cell3.innerHTML = '<button onclick="deleteTask(this)">&#x2715</button>';
+    }
+  }
+  statusBar();
+}
+
+function showComplete() {
+  let tasks = document.getElementById("taskList");
+  tasks.innerHTML = "";
+  for (let x = 0; x < toDoList.length; x++) {
+    let tasks = document.getElementById("taskList");
+
+    let rowCount = tasks.rows.length;
+    let row = tasks.insertRow(rowCount);
+
+    if (toDoList[x].completed) {
+      let cell1 = row.insertCell(0);
+      cell1.innerHTML =
+        '<input type="checkbox" onclick="updateTask()" checked>';
+
+      let cell2 = row.insertCell(1);
+      cell2.innerHTML =
+        '<p class="completedTask">' + toDoList[x].content + "</p>";
+
+      let cell3 = row.insertCell(2);
+      cell3.innerHTML = '<button onclick="deleteTask(this)">&#x2715</button>';
+    }
   }
   statusBar();
 }
@@ -37,7 +92,9 @@ function statusBar() {
   let endCell = row2.insertCell();
   endCell.setAttribute("colspan", 3);
   endCell.innerHTML =
-    '<p>0 Tasks left</p><button onclick="showAll()">All</button><button onclick="#">Active</button><button onclick="#">Completed</button>';
+    "<p>" +
+    tasksLeft() +
+    ' Tasks left</p><button onclick="showAll()">All</button><button onclick="showActive()">Active</button><button onclick="showComplete()">Completed</button>';
 
   if (toDoList.length === 0) {
     tasks.deleteRow(0);
@@ -45,8 +102,19 @@ function statusBar() {
 }
 
 function showAll() {
+  let tasks = document.getElementById("taskList");
   tasks.innerHTML = "";
   displayList();
+}
+
+function tasksLeft() {
+  let activeTasks = 0;
+  for (let x = 0; x < toDoList.length; x++) {
+    if (toDoList[x].completed) {
+      activeTasks++;
+    }
+  }
+  return activeTasks;
 }
 
 function updateTask() {
@@ -65,6 +133,7 @@ function updateTask() {
       toDoList[x].completed = false;
     }
   }
+  tasksLeft();
 }
 
 function deleteTask(deleteBtn) {
