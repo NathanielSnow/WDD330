4ba8f52105951b48e4364531a53684e8908c5a08
NathanielSnow <sno20001@byui.edu>
2021-10-29 11:37:38 -0500

    Update comments.js

--- Week 7/exercises/js/comments.js
+++ Week 7/exercises/js/comments.js
@@ -1,70 +1,37 @@
-const CommentsList = [
-  {
-    name: "Bechler Falls",
-    date: "2001-06-27",
-    content:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam dolores corrupti vitae alias error amet nostrum eius earum cumque totam inventore perferendis vel culpa pariatur, maiores dolorem laudantium, veritatis tempore.",
-    type: "hikes",
-  },
-  {
-    name: "Bechler Falls",
-    date: "2001-06-28",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla autem consectetur voluptates quis odio, odit sint et pariatur praesentium reprehenderit id neque perspiciatis libero ea itaque nesciunt eveniet a corporis.",
-    type: "hikes",
-  },
-  {
-    name: "Teton Canyon",
-    date: "2001-06-27",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni et in vel a, quas provident dicta nulla, veritatis ducimus, natus ut cumque cupiditate dolorem ad. Excepturi sit sed doloribus laudantium?",
-    type: "hikes",
-  },
-];
-
-var cl = [];
-
-//const CommentsList = JSON.parse(localStorage.getItem('CommentsList'));
-
-/*localStorage.removeItem('CommentsList');
-    localStorage.setItem('CommentsList', JSON.stringify(CommentsList));
-    JSON.parse(localStorage.getItem('CommentsList'));
-    */
-
 var type = ""; // dellete may be
 export default class Comments {
   constructor(elementId, typeOfComments) {
+    console.log("elementId:", elementId);
+    console.log("typeOfComments:", typeOfComments);
     this.parentElement = document.getElementById(elementId);
     this.type = typeOfComments;
+    console.log("this.type:", this.type);
+    this.cl = getFromLocalStorge(this.type) || [];
+    console.log("this.cl:", this.cl);
   }
 
   showCommentsList(typeOfFilter) {
     if (typeOfFilter == "all") {
-      this.renderCommentList(this.getAllComments());
+      this.renderCommentList(this.getAllComments(), typeOfFilter);
     } else {
-      this.renderCommentList(this.filterCommentsByName(typeOfFilter));
+      this.renderCommentList(
+        this.filterCommentsByName(typeOfFilter),
+        typeOfFilter
+      );
     }
   }
 
   getAllComments() {
-    let answer = cl.filter((comment) => comment.type === this.type);
-    //console.log("answer get all comments: ", answer);
-    return answer;
+    return this.cl;
   }
 
-  getToday() {
-    const d = new Date();
-    let today =
-      toString(d.getMonth()) +
-      "-" +
-      toString(d.getDay) +
-      "-" +
-      toString(d.getFullYear);
-    return today;
+  filterCommentsByName(typeOfFilter) {
+    return this.cl.filter((comment) => comment.name === typeOfFilter);
   }
 
-  renderCommentList(listOfCommentsToRender) {
-    //console.log("listOfCommentsToRender: ",listOfCommentsToRender);
+  renderCommentList(listOfCommentsToRender, typeOfFilter) {
+    console.log("listOfCommentsToRender: ", listOfCommentsToRender);
+    console.log("ttypeOfFilter: ", typeOfFilter);
 
     let listOfComments = document.createElement("ul");
     listOfCommentsToRender.forEach((comment) => {
@@ -82,53 +49,65 @@ export default class Comments {
 
       listOfComments.appendChild(individualComment); // add the line
     });
-    //this.parentElement.innerHTML = "";
+    this.parentElement.innerHTML = "";
+    let title = document.createElement("h1");
+    title.innerText = "Comments";
+    this.parentElement.appendChild(title);
     this.parentElement.appendChild(listOfComments); // add to the list
 
-    let addNewDiv = document.createElement("div");
     //addNewDiv.id = "addNewDiv";
 
-    let newCommentInput = document.createElement("input");
-    newCommentInput.type = "text";
+    if (typeOfFilter !== "all") {
+      let addNewDiv = document.createElement("div");
+      addNewDiv.id = "addNewDiv";
+      let newCommentInput = document.createElement("input");
+      newCommentInput.type = "text";
 
-    let addCommentInputButton = document.createElement("input");
-    addCommentInputButton.type = "button";
-    addCommentInputButton.value = "+";
+      let addCommentInputButton = document.createElement("input");
+      addCommentInputButton.type = "button";
+      addCommentInputButton.value = "+";
 
-    addCommentInputButton.addEventListener("click", () => {
-      this.addComment("hike", this.getToday(), newCommentInput.value, "hike");
-    });
+      addCommentInputButton.addEventListener("click", () => {
+        this.addComment(newCommentInput.value, typeOfFilter);
+      });
 
-    addNewDiv.appendChild(newCommentInput);
-    addNewDiv.appendChild(addCommentInputButton);
+      addNewDiv.appendChild(newCommentInput);
+      addNewDiv.appendChild(addCommentInputButton);
 
-    this.parentElement.appendChild(addNewDiv);
+      this.parentElement.appendChild(addNewDiv);
+    }
   }
 
-  filterCommentsByName(typeOfFilter) {
-    return cl.filter(
-      (comment) => comment.type === this.type && comment.name === typeOfFilter
-    );
-  }
-
-  /// WORKING HERE
-  addComment(commentName, commentDate, commentContent, commentType) {
+  addComment(newComment, nameOfComment) {
     var c = {
-      name: commentName,
-      date: commentDate,
-      content: commentContent,
-      type: commentType,
+      name: nameOfComment,
+      date: getToday(),
+      content: newComment,
+      type: this.type,
     };
-    this.addLocalSotrage(c);
-    this.getFromLocalStorge();
+    this.cl.push(c);
+    console.log("cl: ", this.cl);
+    addLocalSotrage(this.type, this.cl);
+    this.showCommentsList(nameOfComment);
   }
+}
 
-  addLocalSotrage(comment) {
-    localStorage.setItem("CommentsList", JSON.stringify(comment));
-  }
+function getToday() {
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, "0");
+  let mm = String(today.getMonth() + 1).padStart(2, "0");
+  let yyyy = today.getFullYear();
 
-  getFromLocalStorge() {
-    cl = JSON.parse(localStorage.getItem("CommentsList"));
-    console.log("getlocalstorage: ", cl);
-  }
+  today = yyyy + "-" + mm + "-" + dd;
+  return today;
+}
+
+function addLocalSotrage(key, data) {
+  console.log("key in LS:", key);
+  console.log("data in LS:", data);
+  localStorage.setItem(key, JSON.stringify(data));
+}
+
+function getFromLocalStorge(key) {
+  return JSON.parse(window.localStorage.getItem(key));
 }
