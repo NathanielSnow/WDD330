571fad1f179d2f372bf50fb0ac4f14163a1bd08c
NathanielSnow <sno20001@byui.edu>
2021-11-05 01:37:43 -0500

    Update teamAssignment.js

--- Week 8/exercises/js/teamAssignment.js
+++ Week 8/exercises/js/teamAssignment.js
@@ -12,23 +12,118 @@ function fetchData(dataLink) {
     .then((list) => list.json())
     .then(
       (data) =>
-        (container.innerHTML = displayData(data) + "<br>" + displayBtns(data))
+        (container.innerHTML =
+          displayData(data, dataLink) + "<br>" + displayBtns(data))
     );
 }
 
+function fetchPersonData(dataLink) {
+  fetch(dataLink)
+    .then((list) => list.json())
+    .then((data) => (container.innerHTML = displayPersonData(data)));
+}
+
 //grabs names from object and returns string of names
-function displayData(obj) {
-  console.log(obj);
+function displayPersonData(obj) {
+  let dataNum = JSON.stringify(obj.url).match(/(\d+)/);
+  let pageNum = 0;
+
+  if (dataNum[0] >= 0 && dataNum[0] <= 10) {
+    pageNum = 1;
+  } else if (dataNum[0] >= 11 && dataNum[0] <= 21) {
+    pageNum = 2;
+  } else if (dataNum[0] >= 22 && dataNum[0] <= 31) {
+    pageNum = 3;
+  } else if (dataNum[0] >= 32 && dataNum[0] <= 41) {
+    pageNum = 4;
+  } else if (dataNum[0] >= 42 && dataNum[0] <= 51) {
+    pageNum = 5;
+  } else if (dataNum[0] >= 52 && dataNum[0] <= 61) {
+    pageNum = 6;
+  } else if (dataNum[0] >= 62 && dataNum[0] <= 71) {
+    pageNum = 7;
+  } else if (dataNum[0] >= 72 && dataNum[0] <= 81) {
+    pageNum = 8;
+  } else if (dataNum[0] >= 82 && dataNum[0] <= 83) {
+    pageNum = 9;
+  }
+
+  return (
+    JSON.stringify(obj.name + " " + obj.gender) +
+    "<br>" +
+    `<button onclick=fetchData("https://swapi.dev/api/people/?page=${pageNum}")>Back</button>`
+  );
+}
+
+//grabs names from object and returns string of names
+function displayData(obj, dataLink) {
   let people = [];
+  let pageNum = dataLink.match(/(\d+)/);
+  let varNum = 0;
+  let offNum = 0;
+
+  switch (pageNum[0]) {
+    case "1":
+      varNum = 0;
+      offNum = 1;
+      break;
+    case "2":
+      varNum = 10;
+      offNum = 1;
+      break;
+    case "3":
+      varNum = 20;
+      offNum = 2;
+      break;
+    case "4":
+      varNum = 30;
+      offNum = 2;
+      break;
+    case "5":
+      varNum = 40;
+      offNum = 2;
+      break;
+    case "6":
+      varNum = 50;
+      offNum = 2;
+      break;
+    case "7":
+      varNum = 60;
+      offNum = 2;
+      break;
+    case "8":
+      varNum = 70;
+      offNum = 2;
+      break;
+    case "9":
+      varNum = 80;
+      offNum = 2;
+      break;
+    default:
+      varNum = 0;
+      offNum = 0;
+      break;
+  }
+
   let objSize = obj.results.length;
 
   for (let i = 0; i < objSize; i++) {
-    people.push(
-      "<a class='personInfo()'>" + JSON.stringify(obj.results[i].name) + "</a>"
-    );
+    if (pageNum[0] == 2 && i >= 6) {
+      people.push(
+        `<p onclick="fetchPersonData('https://swapi.dev/api/people/${
+          i + 2 + varNum
+        }/')">${JSON.stringify(obj.results[i].name)}</p>`
+      );
+    } else {
+      people.push(
+        `<p onclick="fetchPersonData('https://swapi.dev/api/people/${
+          i + offNum + varNum
+        }/')">${JSON.stringify(obj.results[i].name)}</p>`
+      );
+    }
   }
 
-  return people.join(", <br>");
+  return people.join("<br>");
 }
 
 //displays buttons with link to the next or pervious pages
